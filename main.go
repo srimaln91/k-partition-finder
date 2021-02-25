9b8642dd1f7632e4bdaf92ec8e0ec6b2ccc3417c
srimal_n <srimal@pickme.lk>
2021-02-25 15:02:29 +0530

    Added number of partitions as a parameter

--- main.go
+++ main.go
@@ -9,25 +9,27 @@ import (
 
 func main() {
 
-	keyPtr := flag.String("key", "", "Key")
+	topic := flag.String("key", "", "Key")
+	numOfPartitions := flag.Int("partitions", 12, "Partitions")
 
 	flag.Parse()
 
-	if keyPtr == nil || *keyPtr == "" {
+	if topic == nil || *topic == "" {
 		fmt.Println("Invalid Key")
 		return
 	}
 
 	message := sarama.ProducerMessage{
 		Topic: "trip",
-		Key:   sarama.StringEncoder(*keyPtr),
+		Key:   sarama.StringEncoder(*topic),
 		Value: nil,
 	}
 
 	fmt.Println("Using default partitioner: Hash")
 
 	partitioner := sarama.NewHashPartitioner("trip")
-	partition, _ := partitioner.Partition(&message, 12)
+	partition, _ := partitioner.Partition(&message, int32(*numOfPartitions))
+
 	fmt.Println(fmt.Sprintf("Partition: %d", partition))
 
 	return
